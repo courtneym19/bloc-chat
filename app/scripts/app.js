6cbe97b2a08d0eb3ad597958cb7fdcd2cd2ff572
Courtney Miller <courtneymiller919@gmail.com>
2017-03-23 14:41:39 -0600

    Bloc Chat Initial setup revision

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,19 @@
-var app = angular.module('blocChat', ['ui.router', 'firebase']);
+(function() {
+     function config($stateProvider, $locationProvider) {
+         $locationProvider
+             .html5Mode({
+                 enabled: true,
+                 requireBase: false
+             });
+         $stateProvider
+             .state('main', {
+                 url: '/',
+                 controller: 'MainCtrl as main',
+                 templateUrl: '/templates/main.html'
+             })
+     }
+ 
+     angular
+         .module('blocChat', ['ui.router', 'firebase'])
+         .config(config);
+ })();
