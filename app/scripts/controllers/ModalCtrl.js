1a9ff62b194bc2dbe15a30b1c462b470eaa0a46f
Courtney Miller <courtneymiller919@gmail.com>
2017-04-04 12:05:27 -0600

    create rooms almost there

---
+++ app/scripts/controllers/ModalCtrl.js
@@ -0,0 +1,19 @@
+(function(){
+    function ModalCtrl(Room, $uibModalInstance){
+        
+        this.name = '';
+        
+        this.submit = function(){
+            $uibModalInstance.close(this.name);
+        }
+        
+        this.cancel = function(){
+            $uibModalInstance.dismiss('cancel');
+        }
+    }
+    
+    angular
+        .module('blocChat')
+        .controller('ModalCtrl', ['Room', '$uibModalInstance', ModalCtrl]);
+    
+})();
