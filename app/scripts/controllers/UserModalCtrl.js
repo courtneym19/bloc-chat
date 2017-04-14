6afdca890c67438ed3933229fd90472acf527062
Courtney Miller <courtneymiller919@gmail.com>
2017-04-13 20:43:00 -0600

    username alert fix

--- app/scripts/controllers/UserModalCtrl.js
+++ app/scripts/controllers/UserModalCtrl.js
@@ -1,14 +1,18 @@
 (function(){
     function UserModalCtrl($cookies, $uibModalInstance){
         
+        var username = '';
+        
         this.submit = function(username){
-            if (username.length >= 1) {
+            if (username === undefined){
+               alert("Please enter a username"); 
+            }
+            
+            else if (username.length >= 1) {
                 $cookies.put('blocChatCurrentUser', username);
                 $uibModalInstance.close();
             }
-            else {
-               alert("Please enter a username"); 
-            }
+      
         }
 
 
